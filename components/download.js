83a4e5d6900d9c77cc9948791fa1ed3e2a2ef8d0
Florian Delezenne <del.florian@gmail.com>
2017-05-12 17:02:31 +0200

    Button

--- components/download.js
+++ components/download.js
@@ -82,12 +82,9 @@ class Download extends React.Component {
             text-align: center;
             outline: 0;
             cursor: pointer;
-            width: 180px;
             border: none;
             border-radius: 2px;
-            min-height: 45px;
-            line-height: 45px;
-            padding: 0 2rem;
+            padding: 10px 20px;
             -webkit-tap-highlight-color: transparent;
             box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);
             background-color: #26a69a;
