74b493e3566d5c857286e3e4dbbe69f3251824b7
MargotDem <margot_demaulmont@hotmail.fr>
2017-07-04 12:25:25 +0200

    Update mobilisco.js
    lien vers forum et vidéo

--- pages/mobilisco.js
+++ pages/mobilisco.js
@@ -6,6 +6,7 @@ import Download from '../components/download'
 import SalesPoints from '../components/sales-points'
 import Corporate from '../components/corporate'
 import HowToSteps from '../components/how-to-steps'
+import ExternalLinks from '../components/external-links'
 import { Products } from '../components/constants'
 import { Colors } from '../components/constants'
 
@@ -40,6 +41,8 @@ const Mobilisco = () => (
         ]}
       />
 
+  <ExternalLinks constant={constant} />
+  
       <Corporate
         simple="Comme tous les outils Open Académie, Mobilisco vise à simplifier concrètement vos opérations de gestion des établissements scolaires. Tout en gardant les outils que vous connaissez, c’est la fonctionnalité (plutôt que l’apparence) qui est repensée."
         gratuit="Elaboré par des agents, pour des agents, Mobilisco est libre et gratuit. Cette liberté est matérialisée par la licence GPL, utilisée par la majorité des logiciels libres (Linux, Firefox, Libre Office...)."
