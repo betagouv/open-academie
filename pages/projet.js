9ac628cb6fbdeaa1b12c2624b319c89dad6d285b
elizlp <elise.levy@sciencespo.fr>
2017-07-04 15:42:01 +0200

    Update projet.js

--- pages/projet.js
+++ pages/projet.js
@@ -40,7 +40,7 @@ const Projet = () => (
 
         <style jsx>{`
           .why {
-            background-image: url('../static/chair.jpg');
+            background-image: url('../static/Leprojet.jpg');
             background-attachment: fixed;
             background-position: center -125px;
             background-size: cover;
