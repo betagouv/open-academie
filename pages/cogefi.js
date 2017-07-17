bbb6f45c6df9c13c9bc779271e46f6515c989bfb
MargotDem <margot_demaulmont@hotmail.fr>
2017-07-17 16:36:02 +0200

    Update corporate cogefi.js

--- pages/cogefi.js
+++ pages/cogefi.js
@@ -72,7 +72,7 @@ const Cogefi = () => (
 
       <Corporate
         simple="Comme tous les outils Open Académie, Cogefi vise à simplifier concrètement vos opérations de gestion des établissements scolaires. Tout en gardant les outils que vous connaissez, c’est la fonctionnalité (plutôt que l’apparence) qui est repensée."
-        gratuit="Elaboré par des agents, pour des agents, Cogefi est libre et gratuit. Cette liberté est matérialisée par la licence GPL, utilisée par la majorité des logiciels libres (Linux, Firefox, Libre Office...)."
+        gratuit="Elaboré par des agents du service public pour eux-mêmes et leurs collègues, Cogefi est libre et gratuit. Cette liberté est matérialisée par la licence GPL, utilisée par la majorité des logiciels libres (Linux, Firefox, Libre Office...)."
       />
 
 
