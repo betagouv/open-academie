3055fd2fa9ff28831c303f8fd33cf2784b09b868
Florian Delezenne <del.florian@gmail.com>
2017-12-05 11:33:23 +0100

    Add next static export

---
+++ next.config.js
@@ -0,0 +1,11 @@
+exports.exportPathMap = () => ({
+  "/": { page: "/" },
+  "/bourse": { page: "/bourse" },
+  "/contact": { page: "/contact" },
+  "/emaintenance": { page: "/emaintenance" },
+  "/gemasco": { page: "/gemasco" },
+  "/mobilisco": { page: "/mobilisco" },
+  "/outils": { page: "/outils" },
+  "/projet": { page: "/projet" },
+  "/votre-projet": { page: "/votre-projet" }
+})
