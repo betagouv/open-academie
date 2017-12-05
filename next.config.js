e72f3c0d468195b1fda317bdb3041830725d8054
Florian Delezenne <del.florian@gmail.com>
2017-12-05 14:41:47 +0100

    Going to branch

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
-exports.exportPathMap = () => ({
-  "/": { page: "/" },
-  "/bourse": { page: "/bourse" },
-  "/contact": { page: "/contact" },
-  "/emaintenance": { page: "/emaintenance" },
-  "/gemasco": { page: "/gemasco" },
-  "/mobilisco": { page: "/mobilisco" },
-  "/outils": { page: "/outils" },
-  "/projet": { page: "/projet" },
-  "/votre-projet": { page: "/votre-projet" }
-})
+module.exports = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/bourse": { page: "/bourse" },
+      "/contact": { page: "/contact" },
+      "/emaintenance": { page: "/emaintenance" },
+      "/gemasco": { page: "/gemasco" },
+      "/mobilisco": { page: "/mobilisco" },
+      "/outils": { page: "/outils" },
+      "/projet": { page: "/projet" },
+      "/votre-projet": { page: "/votre-projet" }
+    }
+  }
+}
