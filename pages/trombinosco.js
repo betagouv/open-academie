65e6601690061e4772386c2244972dd23f66d6cf
Florian Delezenne <del.florian@gmail.com>
2018-04-08 12:47:50 +0200

    Ajout page trombinosco

---
+++ pages/trombinosco.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Brief from '../components/brief'
+import ProductHead from '../components/product-head'
+import ButtonLink from '../components/button-link'
+import SalesPoints from '../components/sales-points'
+import Corporate from '../components/corporate'
+import HowToSteps from '../components/how-to-steps'
+import ExternalLinks from '../components/external-links'
+import Iframe from '../components/iframe'
+import { Products } from '../components/constants'
+import { Colors } from '../components/constants'
+
+const constant = Products.find(product => product.id === 'trombinosco')
+
+const Trombinosco = () => (
+  <Layout title={constant.title}>
+    <main>
+      <ProductHead constant={constant} />
+
+      <div className="wrapper">
+        <Brief
+          description={constant.description}
+          downloadComponent={<ButtonLink href={constant.downloadUrl} download={true} text="Télécharger" className="primary" />} />
+
+        <SalesPoints points={constant.salesPoints} />
+
+        <style jsx>{`
+          .wrapper {
+            display: flex;
+            flex-wrap: wrap;
+          }
+        `}</style>
+      </div>
+
+      <HowToSteps
+        steps={[
+          'Télécharger',
+          'Importer la base élèves',
+          'Saisir les constantes de l\'établissement',
+          'Indiquer l\'emplacement de la collection de photos'
+        ]}
+      />
+
+      <ExternalLinks constant={constant} />
+
+      <Corporate
+        simple="Comme tous les outils Open Académie, Trombinosco vise à simplifier concrètement vos opérations de gestion des établissements scolaires. TrombinoSCO professionnalise la gestion des photos des élèves, au bénéfice de la vie scolaire, des enseignants et du pôle administratif."
+        gratuit="Elaboré par des agents du service public pour eux-mêmes et leurs collègues, Trombinosco est libre et gratuit. Cette liberté est matérialisée par la licence GPL, utilisée par la majorité des logiciels libres (Linux, Firefox, LibreOffice...)."
+      />
+    </main>
+  </Layout>
+)
+
+export default Trombinosco
