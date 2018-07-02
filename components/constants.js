b2538c6dadfa8e7fe1f6f67aa1de654c7fcdc54b
L'intendant zonard - Etienne <etienne@intendancezone.net>
2018-07-02 18:45:06 +0200

    Lien vers Gemasco 2.0

--- components/constants.js
+++ components/constants.js
@@ -73,11 +73,11 @@ export const Products = [
   {
     id: 'gemasco',
     page: '/gemasco',
-    title: 'Gemasco',
+    title: 'Gemasco 2.0',
     formUrl: 'https://docs.google.com/forms/d/1AcTYcJrqYufC7N8SUeWzC1EpID0kprPHjTNq8Z5JOuk/formResponse?entry.1936573090=',
     emailEntryRef: '843103382',
     pageEntryRef: '89190430',
-    downloadUrl: '/static/files/gemasco_v1-8-2.zip',
+    downloadUrl: '/static/files/gemasco_v2-0.zip',
     tagline: 'L’appli qui gère les manuels scolaires',
     description: 'Gemasco commande, distribue, récupère et fait même l’inventaire de vos manuels scolaires !',
     imageSource: '/static/book.png',
