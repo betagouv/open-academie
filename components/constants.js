5fb9f2535094f199db67400262d7b19eeaabed21
L'intendant zonard - Etienne <etienne@intendancezone.net>
2020-04-24 00:21:31 +0200

    Mise en prod GemaSCO 3.0.0

--- components/constants.js
+++ components/constants.js
@@ -73,11 +73,11 @@ export const Products = [
   {
     id: 'gemasco',
     page: '/gemasco',
-    title: 'GemaSCO 2.2.4',
+    title: 'GemaSCO 3.0.0',
     formUrl: 'https://docs.google.com/forms/d/1AcTYcJrqYufC7N8SUeWzC1EpID0kprPHjTNq8Z5JOuk/formResponse?entry.1936573090=',
     emailEntryRef: '843103382',
     pageEntryRef: '89190430',
-    downloadUrl: '/static/files/gemasco_v2-2-4.zip',
+    downloadUrl: '/static/files/gemasco_3-0-0.zip',
     tagline: 'L’appli qui gère les manuels scolaires',
     description: 'GemaSCO commande, distribue, récupère et fait même l’inventaire de vos manuels scolaires !',
     imageSource: '/static/book.png',
