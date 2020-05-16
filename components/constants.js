8a3aeb82b76037d5f0aaf8669138680732ff923d
L'intendant zonard - Etienne <etienne@intendancezone.net>
2020-05-16 21:20:09 +0200

    Update constants.js

--- components/constants.js
+++ components/constants.js
@@ -4,7 +4,7 @@ export const Products = [
     page: '/mobilisco',
     title: 'MobiliSCO 7.2',
     formUrl: 'https://docs.google.com/forms/d/1NmOvwDBvewcF_F68_tsugW_jv_4_5aX98mVk-bajzdY/formResponse?entry.162101201=',
-    downloadUrl: '/static/files/MobiliSCO_V7-2-0',
+    downloadUrl: '/static/files/MobiliSCO_V7-2-0.zip',
     tagline: 'Les voyages scolaires cessent d’être un enfer',
     description: 'Un logiciel de gestion de vos voyages scolaires, depuis l’inscription des élèves jusqu’au bilan financier',
     imageSource: '/static/arrow.png',
